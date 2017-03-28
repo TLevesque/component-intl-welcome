ab2329d4b25aa82f626f847b9f60f818e4544489
Serguei Okladnikov <oklaspec@gmail.com>
2017-03-29 02:24:38 +0400

    wrap form in table

--- src/index.js
+++ src/index.js
@@ -20,34 +20,63 @@ class Welcome extends React.Component {
 
   render() {
     return (
-      <div>
+      <div class="main-content">
 
-        <FormattedMessage
-          id="component-intl-welcome.your-name"
-          defaultMessage="Your name"/>: &nbsp;
-        <input type='text'
-          value={this.state.name}
-          onChange={(ev)=>this.setState({name:ev.target.value})}/> &nbsp;
+        <table style={{border:0}}>
+          <tbody>
 
-        <FormattedMessage
-          id="component-intl-welcome.work-began-year"
-          defaultMessage="work began year"/>: &nbsp;
-        <input type='number'
-          value={this.state.date.getFullYear()}
-          onChange={(ev)=>{
-            let date = new Date(this.state.date)
-            date.setYear(ev.target.value)
-            this.setState({date:date})
-          }}/> &nbsp;
+            <tr>
+              <td>
+                <FormattedMessage
+                  id="component-intl-welcome.your-name"
+                  defaultMessage="Your name"
+               />: &nbsp;
+              </td>
+              <td>
+                <input type='text'
+                  value={this.state.name}
+                  onChange={(ev)=>this.setState({name:ev.target.value})}
+                /> &nbsp;
+              </td>
+            </tr>
 
-        <FormattedMessage
-          id="component-intl-welcome.number-of-works"
-          defaultMessage="number of works"/>: &nbsp;
-        <input type='number'
-          value={this.state.works}
-          onChange={(ev)=>this.setState({works:ev.target.value})}/> &nbsp;
+            <tr>
+              <td>
+                <FormattedMessage
+                  id="component-intl-welcome.work-began-year"
+                  defaultMessage="work began year"
+                />: &nbsp;
+              </td>
+              <td>
+                <input type='number'
+                  value={this.state.date.getFullYear()}
+                  onChange={(ev)=>{
+                    let date = new Date(this.state.date)
+                    date.setYear(ev.target.value)
+                    this.setState({date:date})
+                  }}/> &nbsp;
+              </td>
+            </tr>
 
-        <br/><br/>
+            <tr>
+              <td>
+                <FormattedMessage
+                  id="component-intl-welcome.number-of-works"
+                  defaultMessage="number of works"
+                />: &nbsp;
+              </td>
+              <td>
+                <input type='number'
+                  value={this.state.works}
+                  onChange={(ev)=>this.setState({works:ev.target.value})}
+                /> &nbsp;
+              </td>
+            </tr>
+
+          </tbody>
+        </table>
+
+        <br/>
 
         <FormattedMessage
           id="component-intl-welcome.welcome-message"
